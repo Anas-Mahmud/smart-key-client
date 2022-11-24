6df768c2f62ec62b4c19710d12dd03eafc86873a
Anas <anasmahmud523@gmail.com>
2022-11-24 18:42:50 +0600

    login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className='flex justify-center items-center'>
-            <h2>this is login</h2>
+        <div className='mb-4'>
+            <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 space-y-10 py-12 px-4 sm:px-6 lg:px-8">
+                <div>
+                    <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login to your account</h1>
+                    <p className="mt-2 text-center text-sm text-gray-200">
+                        Or
+                        <Link to={'/signup'} className="font-medium text-indigo-500 border-b border-indigo-600"> register your FREE account </Link>
+                    </p>
+                </div>
+                <div className="max-w-md w-full mx-auto bg-white shadow rounded-lg p-7 py-16 space-y-6">
+                    <div className="flex flex-col">
+                        <label className="text-sm font-bold text-gray-600 mb-1" for="email">Email Address</label>
+                        <input className="border rounded-md bg-white px-3 py-2" type="text" name="email" id="email" placeholder="Enter your Email Address" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-bold text-gray-600 mb-1" for="password">Password</label>
+                        <input className="border rounded-md bg-white px-3 py-2" type="password" name="password" id="password" placeholder="Enter your Password" />
+                    </div>
+                    <div>
+                        <button className="w-full bg-indigo-600 text-white rounded-md p-2">Sign in</button>
+                    </div>
+                    <div className="relative pb-2">
+                        <div className="absolute top-0 left-0 w-full border-b"></div>
+                        <div className="absolute -top-3 left-0 w-full text-center">
+                            <span className="bg-white px-3 text-sm">or continue via</span>
+                        </div>
+                    </div>
+                    <div className="">
+                        <div className="border-2 rounded-md p-3 cursor-pointer hover:border-gray-600 text-center">
+                            <h2 className='font-semibold'>Sign in with Google</h2>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,57 +4,57 @@ import { Link } from 'react-router-dom';
 const SignUp = () => {
     return (
         <div>
-            <main class="flex w-full">
-                <div class="max-h-screen hidden md:block mx-auto">
-                    <img class="object-cover h-full w-full"
+            <main className="flex w-full">
+                <div className="max-h-screen hidden md:block mx-auto">
+                    <img className="object-cover h-full w-full"
                         src="https://img.freepik.com/premium-vector/woman-with-laptop-sitting-office-desk-holding-pen-while-thinking-about-something-concept-illustration_270158-373.jpg"
                         alt="background" />
                 </div>
-                <div class="w-full md:w-4/6 lg:w-3/5 xl:w-2/5 p-10 min-h-screen flex flex-col justify-center">
-                    <div class="mb-10">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-indigo-600" viewBox="0 0 20 20"
+                <div className="w-full md:w-4/6 lg:w-3/5 xl:w-2/5 p-10 min-h-screen flex flex-col justify-center">
+                    <div className="mb-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-indigo-600" viewBox="0 0 20 20"
                             fill="currentColor">
                             <path fill-rule="evenodd"
                                 d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <h1 class="mt-10 text-5xl font-extrabold">Register</h1>
+                        <h1 className="mt-10 text-5xl font-extrabold">Register</h1>
                     </div>
-                    <div class="w-full mx-auto space-y-6">
-                        <div class="flex flex-col">
-                            <label class="text-sm font-bold text-gray-600 mb-1" for="email">Your Name</label>
+                    <div className="w-full mx-auto space-y-6">
+                        <div className="flex flex-col">
+                            <label className="text-sm font-bold text-gray-600 mb-1" for="email">Your Name</label>
                             <input type="text" name="name" id="name"
-                                class="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
+                                className="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
                                 placeholder="Your name" />
                         </div>
-                        <div class="flex flex-col">
-                            <label class="text-sm font-bold text-gray-600 mb-1" for="email">Your Photo</label>
+                        <div className="flex flex-col">
+                            <label className="text-sm font-bold text-gray-600 mb-1" for="email">Your Photo</label>
                             <input type="file" name="photo" id="photo"
-                                class="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500" />
+                                className="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500" />
                         </div>
-                        <div class="flex flex-col">
-                            <label class="text-sm font-bold text-gray-600 mb-1" for="email">Email Address</label>
+                        <div className="flex flex-col">
+                            <label className="text-sm font-bold text-gray-600 mb-1" for="email">Email Address</label>
                             <input type="text" name="email" id="email"
-                                class="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
+                                className="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
                                 placeholder="jonn@doe.com" />
                         </div>
-                        <div class="flex flex-col">
-                            <label class="text-sm font-bold text-gray-600 mb-1" for="password">Password</label>
+                        <div className="flex flex-col">
+                            <label className="text-sm font-bold text-gray-600 mb-1" for="password">Password</label>
                             <input type="password" name="password" id="password"
-                                class="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
+                                className="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
                                 placeholder="******" />
                         </div>
                         <div>
-                            <button class="w-full bg-indigo-600 text-white rounded-md p-2">Sign Up</button>
+                            <button className="w-full bg-indigo-600 text-white rounded-md p-2">Sign Up</button>
                         </div>
-                        <div class="relative pb-2">
-                            <div class="absolute top-0 left-0 w-full border-b"></div>
-                            <div class="absolute -top-3 left-0 w-full text-center">
-                                <span class="bg-white px-3 text-sm">or</span>
+                        <div className="relative pb-2">
+                            <div className="absolute top-0 left-0 w-full border-b"></div>
+                            <div className="absolute -top-3 left-0 w-full text-center">
+                                <span className="bg-white px-3 text-sm">or</span>
                             </div>
                         </div>
-                        <div class="text-center w-full">
-                            <Link href="#" class="font-medium text-indigo-600 border-b border-indigo-600 text-sm">
+                        <div className="text-center w-full">
+                            <Link to={'/login'} className="font-medium text-indigo-600 border-b border-indigo-600 text-sm">
                                 Login to your existing account
                             </Link>
                         </div>
