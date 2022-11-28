7d9ceef0e93406c0ca62053cc471dd43d63ce977
Anas <anasmahmud523@gmail.com>
2022-11-28 22:32:29 +0600

    readme file

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 
 const Banner = () => {
@@ -7,11 +8,11 @@ const Banner = () => {
             <section>
                 <div className="dark:bg-gray-200">
                     <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
-                        <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">Provident blanditiis cum exercitationem</h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+                        <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">Resell your keyboard</h1>
+                        <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">We are always open to expand our reseller and distribution community. Let’s find out how we can work together.</p>
                         <div className="flex flex-wrap justify-center">
-                            <PrimaryButton>Get started</PrimaryButton>
-                            <button type="button" className="px-8 py-3 m-2 text-lg border rounded dark:border-primary dark:text-gray-900 font-semibold">Learn more</button>
+                            <Link to={'/dashboard/addProduct'}><PrimaryButton>Resell</PrimaryButton></Link>
+                            <Link to={'/blog'}><button type="button" className="px-8 py-3 m-2 text-lg border rounded dark:border-primary dark:text-gray-900 font-semibold">Blogs</button></Link>
                         </div>
                     </div>
                 </div>
